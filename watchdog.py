87d8daafef71fd4a8eb7ac3dda024b1e95006365
Ubuntu <ubuntu@allsky2.lxd>
2023-05-01 13:33:45 +0200

    Update files

--- watchdog.py
+++ watchdog.py
@@ -1,24 +1,23 @@
+#!/usr/bin/python3
 import os
 import calendar
 import time
 import subprocess
-import os
+
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+
+
 cmd1= 'systemctl --user restart indi-allsky'
 cmd2= 'systemctl --user restart indiserver'
-
 
 ct = 60
 image = '/var/www/html/allsky/images/latest.jpg'
 
-
 f = os.path.getmtime(image)
 c = calendar.timegm(time.gmtime())
 d = int(c - f)
-#print(f)
-#print(c)
-#print(d)
-
-
 
 while True:
     f = os.path.getmtime(image)
@@ -26,11 +25,11 @@ while True:
     d = int(c - f)
 
     if d-ct > 0:
-        print("Image too old, " + str(d) + " sec., restart service!")
+        logging.info("Image too old, " + str(d) + " sec., restart service!")
         os.system(cmd1)
         # os.system(cmd2)
         time.sleep(240)
     else:
-        print("Image is " + str(d) + " sec. old - ok.")
+        logging.info("Image is " + str(d) + " sec. old - ok.")
     time.sleep(30)
 
